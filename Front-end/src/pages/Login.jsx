d8928a02a927f0cc8a7e9572433a8382c234e931
laurenegigan <laurene.gigan@laplateforme.io>
2026-01-19 16:37:13 +0100

    add login form structure

--- Front-end/src/pages/Login.jsx
+++ Front-end/src/pages/Login.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1>LOGIN PAGE</h1>
+    <div className="auth-container">
+      <h1>Connexion</h1>
+
+      <form>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Entrez votre email"
+          />  
+        </div>
+
+        <div>
+          <label>Mot de passe</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Entrez votre mot de passe"
+          />
+        </div>
+
+        <button type="submit">Se connecter</button>
+      </form>
     </div>
   );
 }
