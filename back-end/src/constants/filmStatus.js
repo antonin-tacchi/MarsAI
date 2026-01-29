3a735f748d3e39a053aee37dff92a17f222107d1
laurenegigan <laurene.gigan@laplateforme.io>
2026-01-29 14:23:55 +0100

    feat: define film status constants and transitions

---
+++ back-end/src/constants/filmStatus.js
@@ -0,0 +1,16 @@
+const FILM_STATUS = {
+    PENDING:"pending",
+    VALIDATED: "validated",
+    SELECTED: "selected",
+};
+
+const ALLOWED_TRANSITIONS = {
+    [FILM_STATUS.PENDING]: [FILM_STATUS.VALIDATED],
+    [FILM_STATUS.VALIDATED]: [FILM_STATUS.SELECTED],
+    [FILM_STATUS.SELECTED]: [],
+};
+
+module.exports = {
+    FILM_STATUS,
+    ALLOWED_TRANSITIONS,
+};
